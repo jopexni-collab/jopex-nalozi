27c0a89164978f589b0fda65b05ac9731a9cdcd3
jopexni-collab <jopex.ni@gmail.com>
2026-07-10 15:45:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 // webu) browser prihvata sve jer je JoPeX HTML servan sa istog servera.
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
